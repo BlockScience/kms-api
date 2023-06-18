47c81565757dc2e5966ce6ed530a4299e72a2281
Orion Reed <orionreedt@gmail.com>
2023-06-18 01:56:03 +0100

    added stats and logs to dashboard

---
+++ frontend/src/components/logTable.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { createStyles, Table, ScrollArea, rem, Anchor } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    position: 'sticky',
+    top: 0,
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+    transition: 'box-shadow 150ms ease',
+
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      bottom: 0,
+      borderBottom: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[2]
+        }`,
+    },
+  },
+
+  scrolled: {
+    boxShadow: theme.shadows.sm,
+  },
+}));
+
+interface LogTableProps {
+  data: { event: string; object: string; user: string; data: any, time: string }[];
+}
+
+export function LogTable({ data }: LogTableProps) {
+  const { classes, cx } = useStyles();
+  const [scrolled, setScrolled] = useState(false);
+
+  const rows = data.map((row) => (
+    <tr key={row.object}>
+      <td>{row.event}</td>
+      <td><Anchor>{row.object}</Anchor></td>
+      <td>{row.user}</td>
+      <td>{row.data}</td>
+      <td>{row.time}</td>
+    </tr>
+  ));
+
+  return (
+    <ScrollArea h={300} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
+      <Table miw={700}>
+        <thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
+          <tr>
+            <th>Event</th>
+            <th>Object</th>
+            <th>User</th>
+            <th>Data</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </ScrollArea>
+  );
+}
--- frontend/src/views/Dashboard.tsx
+++ frontend/src/views/Dashboard.tsx
@@ -1,13 +1,47 @@
 import { SetTitle } from "@/utilities/metadata";
-import { Button, Title } from "@mantine/core";
+import { Box, Button, Stack } from "@mantine/core";
+import { PageTitle } from "@/components/typography";
+import StatsGrid from "@/components/statsGrid";
+import { LogTable } from "@/components/logTable";
+import InlineTag from "@/components/inlineTag";
 
 export default function Dashboard() {
   return (
     <div>
       <SetTitle text='Dashboard' />
-      <Title order={2}>Dashboard</Title>
-      <Button onClick={() => fetch('/api').then(resp => resp.json()).then(data => { alert(data.message) })}>Summon some data!</Button>
-    </div>
+      <Box maw={1000} mx='auto'>
+        <PageTitle>Dashboard</PageTitle>
+        <Stack>
+          <StatsGrid data={[
+            { title: "Tags Added", value: '17', diff: -7 },
+            { title: "Tags Removed", value: '27', diff: 14 },
+            { title: "Queries Completed", value: '31', diff: 12 },
+            { title: "Proposals Submitted", value: '3', diff: 1 },
+          ]} />
+          <LogTable data={[
+            { event: "Tags Changed", object: "8277e0910d750195b448797616e091ad", user: "tealc@sgc.com", data: <div><InlineTag color="green">kms</InlineTag>&nbsp;<InlineTag color="red">cadcad</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8fa14cdd754f91cc6554c9e71929cce7", user: "daniel@sgc.com", data: <div><InlineTag color="green">fluff</InlineTag>&nbsp;<InlineTag color="green">rock</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "d86ec7ac67cf45f6205a8ed9080e6fc1", user: "steve@sgc.com", data: <div><InlineTag color="green">tree</InlineTag>&nbsp;<InlineTag color="red">stick</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8277e0910d750195b448797616e091ad", user: "tealc@sgc.com", data: <div><InlineTag color="green">kms</InlineTag>&nbsp;<InlineTag color="red">cadcad</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8fa14cdd754f91cc6554c9e71929cce7", user: "daniel@sgc.com", data: <div><InlineTag color="green">fluff</InlineTag>&nbsp;<InlineTag color="green">rock</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "d86ec7ac67cf45f6205a8ed9080e6fc1", user: "steve@sgc.com", data: <div><InlineTag color="green">tree</InlineTag>&nbsp;<InlineTag color="red">stick</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8277e0910d750195b448797616e091ad", user: "tealc@sgc.com", data: <div><InlineTag color="green">kms</InlineTag>&nbsp;<InlineTag color="red">cadcad</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8fa14cdd754f91cc6554c9e71929cce7", user: "daniel@sgc.com", data: <div><InlineTag color="green">fluff</InlineTag>&nbsp;<InlineTag color="green">rock</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "d86ec7ac67cf45f6205a8ed9080e6fc1", user: "steve@sgc.com", data: <div><InlineTag color="green">tree</InlineTag>&nbsp;<InlineTag color="red">stick</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8277e0910d750195b448797616e091ad", user: "tealc@sgc.com", data: <div><InlineTag color="green">kms</InlineTag>&nbsp;<InlineTag color="red">cadcad</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8fa14cdd754f91cc6554c9e71929cce7", user: "daniel@sgc.com", data: <div><InlineTag color="green">fluff</InlineTag>&nbsp;<InlineTag color="green">rock</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "d86ec7ac67cf45f6205a8ed9080e6fc1", user: "steve@sgc.com", data: <div><InlineTag color="green">tree</InlineTag>&nbsp;<InlineTag color="red">stick</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8277e0910d750195b448797616e091ad", user: "tealc@sgc.com", data: <div><InlineTag color="green">kms</InlineTag>&nbsp;<InlineTag color="red">cadcad</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8fa14cdd754f91cc6554c9e71929cce7", user: "daniel@sgc.com", data: <div><InlineTag color="green">fluff</InlineTag>&nbsp;<InlineTag color="green">rock</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "d86ec7ac67cf45f6205a8ed9080e6fc1", user: "steve@sgc.com", data: <div><InlineTag color="green">tree</InlineTag>&nbsp;<InlineTag color="red">stick</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8277e0910d750195b448797616e091ad", user: "tealc@sgc.com", data: <div><InlineTag color="green">kms</InlineTag>&nbsp;<InlineTag color="red">cadcad</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "8fa14cdd754f91cc6554c9e71929cce7", user: "daniel@sgc.com", data: <div><InlineTag color="green">fluff</InlineTag>&nbsp;<InlineTag color="green">rock</InlineTag></div>, time: "yesterday" },
+            { event: "Tags Changed", object: "d86ec7ac67cf45f6205a8ed9080e6fc1", user: "steve@sgc.com", data: <div><InlineTag color="green">tree</InlineTag>&nbsp;<InlineTag color="red">stick</InlineTag></div>, time: "yesterday" },
+          ]} />
+          <Button onClick={() => fetch('/api').then(resp => resp.json()).then(data => { console.log(data.message) })}>Summon some data!</Button>
+        </Stack>
+      </Box>
+    </div >
   );
 }
 
