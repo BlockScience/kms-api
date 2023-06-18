517fb07e8f867a68e6b356b7123e9c6aad278928
Orion Reed <orionreedt@gmail.com>
2023-06-18 10:40:19 +0100

    improved InlineTag rendering

--- frontend/src/components/inlineTag.tsx
+++ frontend/src/components/inlineTag.tsx
@@ -1,29 +1,36 @@
-import { Badge, Text, createStyles } from "@mantine/core";
+import { Badge, createStyles, DefaultMantineColor } from "@mantine/core";
 import { PropsWithChildren } from "react";
-import { currentColorScheme, useTheme } from "@/utilities/theme";
 
 const useStyles = createStyles((theme) => ({
-  tag: {
+  badge: {
     borderRadius: theme.radius.sm,
     padding: '0px 3px 0px 3px',
+    height: '1.6em',
     border: '1px solid',
-    backgroundColor: currentColorScheme() === 'dark' ? useTheme().colors.dark[4] : useTheme().colors.gray[2],
-    borderColor: currentColorScheme() === 'dark' ? useTheme().colors.dark[3] : useTheme().colors.gray[3],
-    color: currentColorScheme() === 'dark' ? useTheme().colors.gray[3] : useTheme().colors.dark[6],
-    cursor: 'pointer'
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
+    borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[3],
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[8],
+    cursor: 'pointer',
+  },
+  badgeColored: {
+    borderRadius: theme.radius.sm,
+    padding: '0px 3px 0px 3px',
+    marginTop: '0.3em',
+    height: '1.6em',
+    // borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[3],
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[8],
+    cursor: 'pointer',
   }
 }));
 
 interface InlineTagProps {
-  linkTo?: string;
-  color?: string;
+  color?: DefaultMantineColor
 }
 
 export default function InlineTag(props: PropsWithChildren<InlineTagProps>) {
-  const { classes } = useStyles();
-  const { colors } = useTheme();
-  const bgCol = currentColorScheme() === 'dark' ? colors.gray[8] : colors.gray[2];
-  return (
-    <Badge bg={bgCol} tt="inherit" fw={500} size="lg" className={classes.tag}>{props.children}</Badge>
-  );
+  const style = useStyles();
+  const { background, color } = style.theme.fn.variant({ variant: 'dark', color: props.color });
+  const backgroundDimmed = style.theme.fn.rgba((background || ''), 0.3);
+  const borderLightened = style.theme.fn.lighten(backgroundDimmed, 0.2);
+  return <Badge tt="inherit" fw={500} size="lg" bg={props.color ? backgroundDimmed : ''} style={props.color ? { borderColor: borderLightened, borderWidth: 1 } : {}} color={props.color ? color : ''} className={props.color ? style.classes.badgeColored : style.classes.badge}>{props.children}</Badge>;
 }
