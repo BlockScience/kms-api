cf2c2a510b01fce3629175fffa8f5dbd7e8c098f
Orion Reed <orionreedt@gmail.com>
2023-06-18 01:55:01 +0100

    mocked search page

--- frontend/src/views/Search.tsx
+++ frontend/src/views/Search.tsx
@@ -1,16 +1,22 @@
 import { useLocation, useSearchParams } from "react-router-dom";
 import { SetTitle } from "@/utilities/metadata";
+import { Pagination, Stack, Paper, Container } from "@mantine/core";
 
 export default function Search() {
   const location = useLocation();
   // const [_, setSearchParams] = useSearchParams();
   // setSearchParams({ q: location.state.q });
-  console.log(location.state.q);
-  console.log(location.state);
   return (
     <div>
       <SetTitle text='Search' />
-      Search View...
+      <Stack maw={1000} mx='auto'>
+        <Paper>Search result 1</Paper>
+        <Paper>Search result 2</Paper>
+        <Paper>Search result 3</Paper>
+        <Container>
+          <Pagination total={10} />
+        </Container>
+      </Stack>
     </div>
   );
 }
