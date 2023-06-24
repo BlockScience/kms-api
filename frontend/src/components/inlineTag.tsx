402833e9415bb0594a554a1e66b9b518e5e8142f
Orion Reed <orionreedt@gmail.com>
2023-06-24 23:02:05 +0100

    Made TagSpan simpler and only single color

--- frontend/src/components/inlineTag.tsx
+++ frontend/src/components/inlineTag.tsx
@@ -1,45 +1,24 @@
-import { Badge, createStyles, DefaultMantineColor } from '@mantine/core'
+import { Badge, createStyles } from '@mantine/core'
 import type { VNode } from 'preact'
 const useStyles = createStyles((theme) => ({
   badge: {
     borderRadius: theme.radius.sm,
     padding: '0px 3px 0px 3px',
-    height: '1.6em',
+    marginTop: '0em',
+    height: '1.7em',
     border: '1px solid',
     backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
     borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[3],
     color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[8],
     cursor: 'pointer',
   },
-  badgeColored: {
-    borderRadius: theme.radius.sm,
-    padding: '0px 3px 0px 3px',
-    marginTop: '0.3em',
-    height: '1.6em',
-    // borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[3],
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[8],
-    cursor: 'pointer',
-  },
 }))
 
-interface InlineTagProps {
-  color?: DefaultMantineColor
-  children: VNode[] | VNode | string
-}
-
-export default function InlineTag({ color, children }: InlineTagProps) {
-  const style = useStyles()
+export default function TagSpan({ children }: { children: VNode[] | VNode | string }) {
+  const { classes } = useStyles()
 
   return (
-    <Badge
-      tt='inherit'
-      fw={500}
-      size='lg'
-      bg={color ? style.theme.fn.rgba(color, 0.3) : null}
-      style={color ? { borderColor: style.theme.fn.lighten(color, 0.2), borderWidth: 1 } : {}}
-      color={color || null}
-      className={color ? style.classes.badgeColored : style.classes.badge}
-    >
+    <Badge tt='inherit' fw={500} size='lg' className={classes.badge}>
       {children}
     </Badge>
   )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,17 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    preact(),
+    preact({
+      babel: {
+        // presets: [...],
+        // Your plugins run before any built-in transform (eg: Fast Refresh)
+        plugins: ['@babel/plugin-transform-react-jsx-source'],
+        // Use .babelrc files
+        // babelrc: true,
+        // Use babel.config.js files
+        // configFile: true,
+      },
+    }),
     vitePluginFaviconsInject('src/assets/images/logo-light.svg'),
     tsconfigPaths(),
   ],
