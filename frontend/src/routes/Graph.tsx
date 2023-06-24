8aac075622a8462a234f69dd0cc9daa5e1d999d8
Orion Reed <orionreedt@gmail.com>
2023-06-24 14:59:58 +0100

    added style + example constraints to graph view

--- frontend/src/routes/Graph.tsx
+++ frontend/src/routes/Graph.tsx
@@ -1,47 +1,73 @@
 import { ExperimentalWarning } from '@/components/ExperimentalWarning'
-import { Affix, rem } from '@mantine/core'
+import { Affix, rem, useMantineTheme } from '@mantine/core'
 import CytoscapeComponent from 'react-cytoscapejs'
 import Cola from 'cytoscape-cola'
 import Cytoscape from 'cytoscape'
+import { useState } from 'react'
 
 Cytoscape.use(Cola)
 
-function createGraph(n: number) {
+function createGraph(n: number, prefix = '') {
   const nodes = new Array(n)
   const edges = new Array(n)
   for (let i = 0; i < n; i++) {
     nodes[i] = {
       data: {
-        id: i.toString(),
-        // label: `Node ${i}`,
+        id: `${prefix}${i}`,
       },
     }
     edges[i] = {
       data: {
-        source: Math.floor(Math.sqrt(i)).toString(),
-        target: (i + 1).toString(),
+        source: `${prefix}${Math.floor(Math.sqrt(i))}`,
+        target: `${prefix}${i + 1}`,
       },
     }
   }
-  console.log(nodes.concat(edges))
   edges.pop()
 
   return nodes.concat(edges)
 }
 
 export default function Graph() {
-  const elements = createGraph(200)
+  const [cy, setCy] = useState<Cytoscape.Core | null>(null)
+  const theme = useMantineTheme()
+  const elements = createGraph(100, 'a')
+    .concat(createGraph(0, 'b'))
+    .concat(createGraph(0, 'c'))
+    .concat(createGraph(0, 'd'))
+
+  const stylesheet = [
+    // the stylesheet for the graph
+    {
+      selector: 'node',
+      style: {
+        'background-color':
+          theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[7],
+      },
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        width: 3,
+        'line-color': theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[4],
+        'source-distance-from-node': '3px',
+        'target-distance-from-node': '3px',
+        'curve-style': 'bezier',
+      },
+    },
+  ]
   const layout = {
     name: 'cola',
     animate: true, // whether to show the layout as it's running
+    infinite: true, // whether the simulation should end
+    maxSimulationTime: 0 * 60 * 1000, // max length in ms to run the layout, does not apply if infinite: true
     refresh: 1, // number of ticks per frame; higher is faster but more jerky
-    maxSimulationTime: 30000, // max length in ms to run the layout
     ungrabifyWhileSimulating: false, // so you can't drag nodes during layout
     fit: true, // on every layout reposition of nodes, fit the viewport
     padding: 30, // padding around the simulation
     boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
     nodeDimensionsIncludeLabels: false, // whether labels should be included in determining the space used by a node
-
     // layout event callbacks
     ready: function () {}, // on layoutready
     stop: function () {}, // on layoutstop
@@ -55,8 +81,23 @@ export default function Graph() {
       return 10
     }, // extra spacing around nodes
     flow: undefined, // use DAG/tree flow layout if specified, e.g. { axis: 'y', minSeparation: 30 }
-    alignment: undefined, // relative alignment constraints on nodes, e.g. {vertical: [[{node: node1, offset: 0}, {node: node2, offset: 5}]], horizontal: [[{node: node3}, {node: node4}], [{node: node5}, {node: node6}]]}
-    gapInequalities: undefined, // list of inequality constraints for the gap between the nodes, e.g. [{"axis":"y", "left":node1, "right":node2, "gap":25}]
+    // relative alignment constraints on nodes, e.g. {vertical: [[{node: node1, offset: 0}, {node: node2, offset: 5}]], horizontal: [[{node: node3}, {node: node4}], [{node: node5}, {node: node6}]]}
+    alignment: cy
+      ? {
+          vertical: [
+            [{ node: cy.$id('a0') }, { node: cy.$id('a1') }],
+            [{ node: cy.$id('a1') }, { node: cy.$id('a2') }],
+            [{ node: cy.$id('a2') }, { node: cy.$id('a3') }],
+          ],
+          horizontal: [
+            [{ node: cy.$id('a2') }, { node: cy.$id('a4') }],
+            [{ node: cy.$id('a4') }, { node: cy.$id('a5') }],
+            [{ node: cy.$id('a5') }, { node: cy.$id('a6') }],
+          ],
+        }
+      : undefined,
+    // list of inequality constraints for the gap between the nodes, e.g. [{"axis":"y", "left":node1, "right":node2, "gap":25}]
+    gapInequalities: undefined,
     centerGraph: true, // adjusts the node positions initially to center the graph (pass false if you want to start the layout from the current position)
 
     // different methods of specifying edge length
@@ -70,14 +111,27 @@ export default function Graph() {
     userConstIter: undefined, // initial layout iterations with user-specified constraints
     allConstIter: undefined, // initial layout iterations with all constraints including non-overlap
   }
-  const style = { width: '100%', height: '100%' }
+  cy?.nodes('[id = "a0"],[id = "a2"],[id = "a3"],[id = "a4"],[id = "a5"],[id = "a6"]').style(
+    'background-color',
+    theme.colors.blue[5],
+  )
 
   return (
     <>
       <Affix position={{ top: rem(20), right: rem(20) }}>
         <ExperimentalWarning />
       </Affix>
-      <CytoscapeComponent elements={elements} style={style} layout={layout} />
+      <CytoscapeComponent
+        cy={(cy) => {
+          setCy(cy)
+        }}
+        userPanningEnabled={false}
+        userZoomingEnabled={false}
+        elements={elements}
+        style={{ width: '100%', height: '100%' }}
+        stylesheet={stylesheet}
+        layout={layout}
+      />
     </>
   )
 }
