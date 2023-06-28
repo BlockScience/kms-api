ea4ae6b4047d54601d67d4b96e2f8e792fc3face
Orion Reed <orionreedt@gmail.com>
2023-06-28 19:45:57 +0100

    setup frontend dev

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,10 +1,12 @@
+const DEV = import.meta.env.MODE === 'development'
+
 export const auth0 = {
   domain: 'dev-67fgpygy2qoenl7r.us.auth0.com',
   audience: 'https://127.0.0.1:8000',
-  clientID: 'BVQY76IBcTjxg0TKDrAXMcM5pL6OW8y2', // needed?
+  clientID: 'BVQY76IBcTjxg0TKDrAXMcM5pL6OW8y2',
   issuerBaseURL: 'something',
 }
 
 export const api = {
-  url: 'api',
+  url: DEV ? 'https://localhost:8000' : 'api',
 }
