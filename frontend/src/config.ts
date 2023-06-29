12f555345dc8586eac3f8ca21fe6abb1eb754eda
Orion Reed <orionreedt@gmail.com>
2023-06-29 14:40:12 +0100

    updated config with new auth domain

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,7 @@
 const DEV = import.meta.env.MODE === 'development'
 
 export const auth0 = {
-  domain: 'dev-67fgpygy2qoenl7r.us.auth0.com',
+  domain: 'auth.kms-beta.block.science',
   audience: 'https://127.0.0.1:8000',
   clientID: 'BVQY76IBcTjxg0TKDrAXMcM5pL6OW8y2',
   issuerBaseURL: 'something',
